2ca31cffaa5958728fef359682c4893ff153a269
tony <osmanetoni@gmail.com>
2025-01-13 20:31:06 -0300

    created various classes that are essential to the base of the project

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.Service.ConsumoAPI;
+import br.com.alura.screenmatch.Service.ConverteDados;
+import br.com.alura.screenmatch.model.DadosSerie;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +18,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=2c1cb14c");
+//		System.out.println(json);
+//		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json,DadosSerie.class);
+		System.out.println(dados);
+
 
 	}
 }
